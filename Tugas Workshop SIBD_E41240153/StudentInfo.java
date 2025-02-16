3cbdaa3f7f11e7414a7d40e7660ec98ee4ccdc44
e41240153-ratualyviameydiandra-a <alyviamey65939@gmail.com>
2025-02-16 12:46:14 +0700

    Create StudentInfo.java

---
+++ Tugas Workshop SIBD_E41240153/StudentInfo.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package studentinfo;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class StudentInfo {
+
+ public static void main(String[] args) {
+        // Deklarasi dan inisialisasi variabel
+        String fName = "Lisa";
+        String lName = "Palombo";
+        String stuId = "123456789";
+        String stuStatus = "Active";
+
+        // Menampilkan informasi siswa
+        System.out.println("Student Name: " + fName + " " + lName);
+        System.out.println("Student ID: " + stuId);
+        System.out.println("Student Status: " + stuStatus);
+    }
+}
