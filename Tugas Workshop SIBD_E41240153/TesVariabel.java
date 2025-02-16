35ae7cf53641528073703164048679aa29721f12
e41240153-ratualyviameydiandra-a <alyviamey65939@gmail.com>
2025-02-16 12:46:18 +0700

    Create TesVariabel.java

---
+++ Tugas Workshop SIBD_E41240153/TesVariabel.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tesvariabel;
+public class TesVariabel {
+
+    public static void main(String[] args) {
+        boolean gameOver = false; //benar, karena nama variabel sesuai dengan aturan  penamaan dan tipe data sesuai dengan nilai false
+        int students = 50, classes = 3; //benar,karena nama variabel mengikuti aturan penamaan, dan deklarasi serta inisialisasi tipe data int benar
+        double sales_tax; //benar,karena nama variabel valid, meskipun belum diinisialisasi langsung
+        short number1; //benar, karena nama variabel sesuai dan boleh mengandung angka di akhir nama variabel
+        
+    //    int 2beOrNot2be;   //Salah, karena nama variabel tidak boleh diawali angka. Nama variabel harus dimulai dengan huruf atau underscore
+    //    float price index; // Salah, karena nama variabel tidak boleh mengandung spasi. Tidak sesuai dengan aturan penulisan variabel
+    //    double lastYear'sPrice; // Salah, karena terdapat tanda apostrof yang tidak diperbolehkan dalam penamaan variabel
+    //    long class;  // Salah, karena class adalah keyword bawaan Java. Maka,keyword ini tidak bisa digunakan sebagai nama variabel
+    }
+}
