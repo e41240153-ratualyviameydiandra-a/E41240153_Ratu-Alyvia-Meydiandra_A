98612036e1f2de940cdb35ac3d94c43cd77971d7
e41240153-ratualyviameydiandra-a <alyviamey65939@gmail.com>
2025-02-23 14:20:13 +0700

    Create Namaarray.java

---
+++ Tugas Workshop SIBD_E41240153/Namaarray.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package namaarray;
+public class Namaarray {
+    public static void main(String[] args) {
+        String[] nama = {"Ratu", "Alyvia", "Meydiandra"};
+        
+        System.out.print("Nama lengkap: ");
+        for (String bagianNama : nama) {
+            System.out.print(bagianNama + " ");
+        }
+        System.out.println(); 
+    }   
+}
