f04af9ab37acb8100995dc341b9a28a21f1921be
e41240153-ratualyviameydiandra-a <alyviamey65939@gmail.com>
2025-03-03 18:39:03 +0700

    Create Animal.java

---
+++ Tugas Workshop SIBD_E41240153/Animal/src/animal/Animal.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package animal;
+
+/**
+ *
+ * @author LENOVO  : Ratu Alyvia Meydiandra
+ */
+public class Animal {
+    public void makeNoise() {
+        System.out.println("talk");
+    }
+}
