e49c2b10236562fee526b23baa155160e948d67d
e41240153-ratualyviameydiandra-a <alyviamey65939@gmail.com>
2025-03-03 17:21:55 +0700

    Create A.java

---
+++ Tugas Workshop SIBD_E41240153/Pewarisan/src/pewarisan/A.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pewarisan;
+
+/**
+ *
+ * @author LENOVO : Ratu Alyvia Meydiandra
+ */
+    public class A {
+        int x;
+        int y;
+
+        void TampilkanNilaiXY() {
+            System.out.println("Nilai X : " + x + ", y : " + y);
+         }
+    }
