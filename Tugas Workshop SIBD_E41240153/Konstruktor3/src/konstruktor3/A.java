f47029118fea2c97f86fc03fc3e288aa331a6884
e41240153-ratualyviameydiandra-a <alyviamey65939@gmail.com>
2025-03-03 17:40:39 +0700

    Create A.java

---
+++ Tugas Workshop SIBD_E41240153/Konstruktor3/src/konstruktor3/A.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package konstruktor3;
+/**
+ * @author LENOVO : Ratu Alyvia Meydiandra
+ */
+public class A {
+    A(String param1) {
+        System.out.println("Konstruktor class A dengan Parameter " + param1 + " dieksekusi ...");
+    }
+}
