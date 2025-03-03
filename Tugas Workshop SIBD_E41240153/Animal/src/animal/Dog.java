3963a770328606d6f2ec96aa7077830e595501a2
e41240153-ratualyviameydiandra-a <alyviamey65939@gmail.com>
2025-03-03 18:39:08 +0700

    Create Dog.java

---
+++ Tugas Workshop SIBD_E41240153/Animal/src/animal/Dog.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package animal;
+
+/**
+ *
+ * @author LENOVO  : Ratu Alyvia Meydiandra
+ */
+public class Dog extends Animal {
+    @Override
+    public void makeNoise() {
+        System.out.println("Bark");
+    }
+}
