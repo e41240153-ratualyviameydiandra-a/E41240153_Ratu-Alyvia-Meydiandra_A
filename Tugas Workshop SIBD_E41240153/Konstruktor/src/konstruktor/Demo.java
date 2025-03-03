bb118bd53f84536b35ebb756d43e63c2eefee7b4
e41240153-ratualyviameydiandra-a <alyviamey65939@gmail.com>
2025-03-03 17:31:18 +0700

    Create Demo.java

---
+++ Tugas Workshop SIBD_E41240153/Konstruktor/src/konstruktor/Demo.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package konstruktor;
+/**
+ *
+ * @author LENOVO : Ratu Alyvia Meydiandra
+ */
+public class Demo {
+    public static void main(String[] args) {
+        C subOb = new C();
+    }
+}
