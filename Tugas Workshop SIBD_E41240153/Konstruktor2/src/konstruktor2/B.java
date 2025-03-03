3e619c865556418c95e6c0d6b26f249ed6afea15
e41240153-ratualyviameydiandra-a <alyviamey65939@gmail.com>
2025-03-03 17:36:10 +0700

    Create B.java

---
+++ Tugas Workshop SIBD_E41240153/Konstruktor2/src/konstruktor2/B.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package konstruktor2;
+/**
+ * @author LENOVO : Ratu Alyvia Meydiandra
+ */
+public class B extends A {
+    B() {
+        // super(); Tambahan kode secara otomatis oleh Java untuk memanggil Konstruktor dari SuperClassnya
+        super(); // Konstruktor default dari class A dipanggil
+        System.out.println("Konstruktor class B dieksekusi ...");
+    }
+}
