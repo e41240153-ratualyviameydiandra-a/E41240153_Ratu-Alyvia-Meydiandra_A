5be998633668a77c2f2171c6ce61384848b680b5
e41240153-ratualyviameydiandra-a <alyviamey65939@gmail.com>
2025-03-03 18:26:50 +0700

    Create A.java

---
+++ Tugas Workshop SIBD_E41240153/Abstract/src/pkgabstract/A.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkgabstract;
+
+/**
+ *
+ * @author LENOVO : Ratu Alyvia Meydiandra
+ */
+public abstract class A {  
+    abstract public void method2(); // Deklarasi method abstrak
+
+    // Method konkrit yang memiliki implementasi
+    public void method1() {
+        System.out.println("Method konkrit dari class A");
+    }
+}
