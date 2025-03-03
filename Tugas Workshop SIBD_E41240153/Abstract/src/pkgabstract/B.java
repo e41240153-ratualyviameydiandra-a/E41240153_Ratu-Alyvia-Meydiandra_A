afc9170841379c2bc3ffab80a0a2654a3a7ac02f
e41240153-ratualyviameydiandra-a <alyviamey65939@gmail.com>
2025-03-03 18:26:54 +0700

    Create B.java

---
+++ Tugas Workshop SIBD_E41240153/Abstract/src/pkgabstract/B.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pkgabstract;
+
+/**
+ *
+ * @author LENOVO : Ratu Alyvia Meydiandra
+ */
+public class B extends A {  
+    // Override method abstrak dan membuat implementasi konkrit
+    public void method2() {  
+        System.out.println("Method abstract yang sudah menjadi konkrit dalam class B");
+    }    
+}
