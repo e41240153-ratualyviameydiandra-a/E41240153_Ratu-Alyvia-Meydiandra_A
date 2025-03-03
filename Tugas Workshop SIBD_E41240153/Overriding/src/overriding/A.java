3a055a7cde1e23aac3f70930c37a35e7fcfaba95
e41240153-ratualyviameydiandra-a <alyviamey65939@gmail.com>
2025-03-03 18:17:13 +0700

    Create A.java

---
+++ Tugas Workshop SIBD_E41240153/Overriding/src/overriding/A.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package overriding;
+/**
+ *
+ * @author LENOVO : Ratu Alyvia Meydiandra
+ */
+public class A {
+    public void tampilkanKeLayar() {
+        System.out.println("Method milik class A dipanggil");
+    }
+}
