6d825f397e4ba272a297bc1c59f71e6f2af359c7
e41240153-ratualyviameydiandra-a <alyviamey65939@gmail.com>
2025-03-03 18:26:58 +0700

    Create Demo.java

---
+++ Tugas Workshop SIBD_E41240153/Abstract/src/pkgabstract/Demo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pkgabstract;
+/**
+ *
+ * @author LENOVO : Ratu Alyvia Meydiandra
+ */
+public class Demo {
+    public static void main(String[] args) {
+        B ob = new B();
+        ob.method1();
+        ob.method2();
+    }    
+}
